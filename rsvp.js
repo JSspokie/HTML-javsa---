03664488ffebd81ec71f86016cee3339b372028a
JSspokie <61015820+JSspokie@users.noreply.github.com>
2024-09-09 12:15:27 +0200

    hhhh

--- rsvp.js
+++ rsvp.js
@@ -1,12 +1,17 @@
 function submitFrm(){
-    saveValues();
-    postValues();
+    if (validateForm()==false){
+        alert("Please fill in all fields");
+        exit();
+    } else {
+        // saveValues();
+        //  postValues();
 
-    window.location.replace("http://127.0.0.1:5500/succ.html")
+        window.location.replace("/succ.html")
+    }
+    
 }
 
-
-function saveValues(){
+function validateForm(){
     var FullName = document.getElementById('FullName').value
     var PhoneNumber = document.getElementById('telnum').value
     var Email = document.getElementById('email').value
@@ -15,11 +20,40 @@ function saveValues(){
     var MenuPref = document.getElementById('MenuPref').value
     var TblOption = document.getElementById('tableOption').value
 
-    localStorage.setItem('fullname', FullName)
-    localStorage.setItem('PhoneNumber', PhoneNumber)
-    localStorage.setItem('Email', Email)
-    localStorage.setItem('NumOfAtt', NumOfAtt)
-    localStorage.setItem('ArrTime', ArrTime)
-    localStorage.setItem('MenuPref', MenuPref)
-    localStorage.setItem('TblOption', TblOption)
+    if(FullName=="" || PhoneNumber=="" || Email=="" || NumOfAtt==""
+        || ArrTime=="" || MenuPref=="" || TblOption==""
+    ){
+        return false;
+    }
+}
+
+function saveValues(){
+    var FullName = document.getElementById('FullName').value;
+    var PhoneNumber = document.getElementById('telnum').value;
+    var Email = document.getElementById('email').value;
+    var NumOfAtt = document.getElementById('numAtt').value;
+    var ArrTime = document.getElementById('arrTime').value;
+    var MenuPref = document.getElementById('MenuPref').value;
+    var TblOption = document.getElementById('tableOption').value;
+
+    localStorage.setItem('fullname', FullName);
+    localStorage.setItem('PhoneNumber', PhoneNumber);
+    localStorage.setItem('Email', Email);
+    localStorage.setItem('NumOfAtt', NumOfAtt);
+    localStorage.setItem('ArrTime', ArrTime);
+    localStorage.setItem('MenuPref', MenuPref);
+    localStorage.setItem('TblOption', TblOption);
+}
+
+function postValues(){
+    const outName = document.getElementById('outName');
+    const outnum = document.getElementById('outnum');
+    const outMail = document.getElementById('outMail');
+    const outNumAtt = document.getElementById('outNumAtt');
+    const outArrTime = document.getElementById('outArrTime');
+    const outMenuPref = document.getElementById('outMenuPref');
+    const outTblOption = document.getElementById('outTblOption');
+
+
+    outName.textContent = localStorage.getItem('fullname');
 }
