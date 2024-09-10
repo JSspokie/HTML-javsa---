72f3b45def05970e238eaab780d687be36eb22e8
JSspokie <61015820+JSspokie@users.noreply.github.com>
2024-09-10 08:05:08 +0200

    hhh

--- rsvp.js
+++ rsvp.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     postValues();
+    confetti();
 });
 
 function submitFrm(){
@@ -72,4 +73,42 @@ function postValues(){
     outArrTime.textContent = "Arrival time: "+localStorage.getItem("ArrTime");
     outMenuPref.textContent = "Menu Preference: "+localStorage.getItem("MenuPref");
     outTblOption.textContent = "Table Option: "+localStorage.getItem("TblOption");
+}
+
+function confetti(){
+    const duration = 99999999 * 999999999,
+    animationEnd = Date.now() + duration;
+  
+  let skew = 1;
+  
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  (function frame() {
+    const timeLeft = animationEnd - Date.now(),
+      ticks = Math.max(200, 500 * (timeLeft / duration));
+  
+    skew = Math.max(0.8, skew - 0.001);
+  
+    confetti({
+      particleCount: 5,
+      startVelocity: 0,
+      ticks: ticks,
+      origin: {
+        x: Math.random(),
+        // since particles fall down, skew start toward the top
+        y: Math.random() * skew - 0.2,
+      },
+      colors: ["#ffffff"],
+      shapes: ["circle"],
+      gravity: randomInRange(0.4, 0.6),
+      scalar: randomInRange(0.4, 1),
+      drift: randomInRange(-0.4, 0.4),
+    });
+  
+    if (timeLeft > 0) {
+      requestAnimationFrame(frame);
+    }
+  })();
 }
